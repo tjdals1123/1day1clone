61ddfa0941cd48f5fd56ec72eaf3d0950a52beb3
tjdals1123 <tjdals1123@gmail.com>
2021-04-28 18:08:32 +0900

    210428

--- src/main/java/org/ict/controller/ReplyController.java
+++ src/main/java/org/ict/controller/ReplyController.java
@@ -2,14 +2,16 @@ package org.ict.controller;
 
 import java.util.List;
 
+
 import org.ict.domain.ReplyVO;
 import org.ict.service.ReplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,7 +47,7 @@ public class ReplyController {
 		return entity;
 	}
 	
-	@PostMapping(value="/all/{bno}", produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	@GetMapping(value="/all/{bno}", produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
 	public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") Integer bno){
 		
 		ResponseEntity<List<ReplyVO>> entity = null;
@@ -61,6 +63,47 @@ public class ReplyController {
 		
 		return entity;
 	}
+	@DeleteMapping(value="/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Integer rno){
+		
+		ResponseEntity<String> entity = null;
+		
+		try {
+			
+			service.removeReply(rno);
+			
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+	}
+	
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH}, value="/{rno}", 
+					consumes = "application/json", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") int rno) {
+		
+		ResponseEntity<String> entity = null;
+		
+		try {
+			
+			vo.setRno(rno);
+			
+			service.modifyReply(vo);
+			
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+		
+	}
+	
 	
 }
 
